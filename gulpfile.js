70404c55af73936b4826fafe412b60f61f1569ca
Ruurd Moelker <RRMoelker@users.noreply.github.com>
2016-09-30 17:19:46 +0200

    Not hoisted function fixed and Travis cache for faster builds.

--- gulpfile.js
+++ gulpfile.js
@@ -6,15 +6,6 @@ const conf = require('./conf/gulp.conf');
 
 require('require-dir')('./gulp_tasks');
 
-gulp.task('build', gulp.series(gulp.parallel('other', 'webpack:dist')));
-gulp.task('watch', watch);
-gulp.task('test:single', gulp.series('eslint', 'karma:single'));
-gulp.task('test:watch', gulp.series('karma:watch'));
-gulp.task('test', gulp.series('test:single'));
-gulp.task('serve', gulp.series('webpack:watch', 'browsersync'));
-gulp.task('serve:dist', gulp.series('clean', 'build', 'browsersync:dist'));
-gulp.task('default', gulp.series('build'));
-
 const reloadBrowserSync = (cb) => {
   browserSync.reload();
   cb();
@@ -24,3 +15,12 @@ const watch = done => {
   gulp.watch(conf.path.tmp('index.html'), reloadBrowserSync);
   done();
 }
+
+gulp.task('build', gulp.series(gulp.parallel('other', 'webpack:dist')));
+gulp.task('watch', watch);
+gulp.task('test:single', gulp.series('eslint', 'karma:single'));
+gulp.task('test:watch', gulp.series('karma:watch'));
+gulp.task('test', gulp.series('test:single'));
+gulp.task('serve', gulp.series('webpack:watch', 'browsersync'));
+gulp.task('serve:dist', gulp.series('clean', 'build', 'browsersync:dist'));
+gulp.task('default', gulp.series('build'));
