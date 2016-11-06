e7e63dc29622ff5c19c0f594db96df9fdb1cba65
Ruurd Moelker <RRMoelker@users.noreply.github.com>
2016-11-06 13:12:11 +0100

    D3 wrapped in react

--- src/app/Graph.js
+++ src/app/Graph.js
@@ -1,36 +1,71 @@
 import React from 'react';
 import {connect} from 'react-redux';
-import {createGraph} from '../d3/D3Graph';
+import D3Graph from '../d3/D3Graph';
 
 class ReactGraph extends React.Component {
 
   constructor(props) {
     super(props);
-    // this.d3Graph = new D3Graph();
+    this.d3Graph = new D3Graph();
   }
 
   _getGraphData() {
-    let nodes = [{id: 'Alice'}, {id: 'Bob'}, {id: 'Eve'}],
-      links = [{source: 'Alice', target: 'Bob'}, {source: 'Eve', target: 'Bob'}];
+    const nodes = [{id: 'a', index: 0}, {id: 'b', index: 1}, {id: 'c', index: 2}];
+    let links = [
+      {source: 'a', target: 'b'},
+      {source: 'a', target: 'c'},
+      {source: 'b', target: 'c'}
+    ];
+    links = this.idToReference(nodes, links);
     return {
       nodes,
       links
     };
   }
 
+  idToReference(nodes, links) {
+    // convert reference by value to reference by reference
+    return links.map(link => {
+      const source = nodes.filter(n => n.id === link.source)[0];
+      const target = nodes.filter(n => n.id === link.target)[0];
+      return {source, target};
+    });
+  }
+
   componentDidMount() {
     const el = this.refs.svg;
-    createGraph(el);
-    // this.d3Graph.create(el, 500, 600, this._getGraphData());
+
+    const data = this._getGraphData();
+
+    // createGraph(el);
+    this.d3Graph.create(el, 960, 500, data);
+
+    // 2. Remove node B and associated links.
+    setTimeout(() => {
+      const data = this._getGraphData();
+      data.nodes.splice(1, 1); // remove b
+      data.links.shift(); // remove a-b
+      data.links.pop(); // remove b-c
+      // let data2 = {
+      //   nodes,
+      //   links
+      // };
+      this.d3Graph.update(data);
+    }, 3000);
+
+  //   // Add node B back.
+    setTimeout(() => {
+        const data = this._getGraphData();
+        this.d3Graph.update(data);
+    }, 6000);
+
   }
 
   componentDidUpdate() {
-    const el = this.refs.svg;
-    // this.d3Graph.update(el, this._getGraphData());
+    // this.d3Graph.update(this._getGraphData());
   }
 
   componentWillUnmount() {
-    const el = this.refs.svg;
     // this.d3Graph.destroy(el);
   }
 
--- src/d3/D3Graph.js
+++ src/d3/D3Graph.js
@@ -1,76 +1,116 @@
 import * as d3 from "d3"; // see https://github.com/d3/d3/blob/master/README.md#installing
 
-export const createGraph = (element) => {
-  let width = 960,
-    height = 500;
-
-  const svg = d3.select(element)
-    .attr("width", width)
-    .attr("height", height);
-
-  // const svg = d3.select("body").append("svg")
-  //     .attr("width", width)
-  //     .attr("height", height);
-
-  const color = d3.scale.category10();
-
-  let nodes = [],
-    links = [];
-
-  const force = d3.layout.force()
-      .nodes(nodes)
-      .links(links)
-      .charge(-400)
-      .linkDistance(120)
-      .size([width, height])
-      .on("tick", tick);
-
-  let node = svg.selectAll(".node"),
-    link = svg.selectAll(".link");
-
-  // 1. Add three nodes and three links.
-  setTimeout(() => {
-    let a = {id: "a"}, b = {id: "b"}, c = {id: "c"};
-    nodes.push(a, b, c);
-    links.push({source: a, target: b}, {source: a, target: c}, {source: b, target: c});
-    start();
-  }, 0);
-
-  // 2. Remove node B and associated links.
-  setTimeout(() => {
-    nodes.splice(1, 1); // remove b
-    links.shift(); // remove a-b
-    links.pop(); // remove b-c
-    start();
-  }, 3000);
-
-  // Add node B back.
-  setTimeout(() => {
-    let a = nodes[0], b = {id: "b"}, c = nodes[1];
-    nodes.push(b);
-    links.push({source: a, target: b}, {source: b, target: c});
-    start();
-  }, 6000);
-
-  function start() {
-    link = link.data(force.links(), d => { return d.source.id + "-" + d.target.id; });
-    link.enter().insert("line", ".node").attr("class", "link");
-    link.exit().remove();
-
-    node = node.data(force.nodes(), d => { return d.id;});
-    node.enter().append("circle").attr("class", d => { return "node " + d.id; }).attr("r", 8);
-    node.exit().remove();
-
-    force.start();
-  }
-
-  function tick() {
-    node.attr("cx", d => { return d.x; })
-        .attr("cy", d => { return d.y; });
-
-    link.attr("x1", d => { return d.source.x; })
-        .attr("y1", d => { return d.source.y; })
-        .attr("x2", d => { return d.target.x; })
-        .attr("y2", d => { return d.target.y; });
-  }
+const createLinkWithReference = (nodes, link) => {
+  // convert reference by value to reference by reference
+  const source = nodes.filter(n => n.id === link.source.id)[0];
+  const target = nodes.filter(n => n.id === link.target.id)[0];
+  console.log("new link", source, target);
+  return {source, target};
 };
+
+const reconcile = (data, newData) => {
+  // update data object to reflect new data.
+  newData.nodes.forEach(newNode => {
+    const node = data.nodes.find(node => node.id === newNode.id);
+    if (node) {
+      // already exists
+    } else {
+      data.nodes.push(newNode);
+    }
+  });
+
+  const nodes2 = data.nodes.filter(node => {
+    return newData.nodes.findIndex(newNode => node.id === newNode.id) !== -1;
+  });
+  // console.log(nodes2);
+  data.nodes = nodes2;
+
+  // remove old links
+  const linksRemoved = data.links.filter(link => {
+    return newData.links.findIndex(newLink =>
+      link.source.id === newLink.source.id &&
+      link.target.id === newLink.target.id
+      ) !== -1;
+  });
+  // console.log(linksRemoved);
+  data.links = linksRemoved;
+
+  // find new links
+  const linksNew = newData.links.filter(newLink => {
+    // return true;
+    return data.links.findIndex(link =>
+      link.source.id === newLink.source.id &&
+      link.target.id === newLink.target.id
+      ) === -1;
+  });
+  linksNew.forEach(link => data.links.push(createLinkWithReference(data.nodes, link)));
+  console.log(linksNew);
+};
+
+export default class D3Graph {
+  create(element, width, height, newData) {
+    this.svg = d3.select(element)
+      .attr("width", width)
+      .attr("height", height);
+
+    // const {nodes, links} = data;
+    this.data = {
+      nodes: [],
+      links: []
+    };
+    reconcile(this.data, newData);
+    const {nodes, links} = this.data;
+
+    const that = this;
+    function tick() {
+      that.nodeGroup
+        .attr("cx", d => d.x)
+        .attr("cy", d => d.y);
+
+      that.linkGroup.attr("x1", d => d.source.x)
+        .attr("y1", d => d.source.y)
+        .attr("x2", d => d.target.x)
+        .attr("y2", d => d.target.y);
+    }
+
+    this.force = d3.layout.force()
+        .nodes(nodes)
+        .links(links)
+        .charge(-400)
+        .linkDistance(120)
+        .size([width, height])
+        .on("tick", tick);
+
+    this.nodeGroup = this.svg.selectAll(".node");
+    this.linkGroup = this.svg.selectAll(".link");
+
+    this._update();
+  }
+
+  _update() {
+    // const {nodes, links} = data;
+    this.linkGroup = this.linkGroup.data(this.force.links(), d => d.source.id + "-" + d.target.id);
+    this.linkGroup.enter().insert("line", ".node").attr("class", "link");
+    this.linkGroup.exit().remove();
+
+    this.nodeGroup = this.nodeGroup.data(this.force.nodes(), d => d.id);
+    this.nodeGroup.enter().append("circle").attr("class", d => `node ${d.id}`).attr("r", 8);
+    this.nodeGroup.exit().remove();
+
+    this.force.start();
+  }
+
+  update(newData) {
+    reconcile(this.data, newData);
+    const {nodes, links} = this.data;
+    this.force
+      .nodes(nodes)
+      .links(links);
+    this._update();
+  }
+
+  destroy() {
+    console.error("destroy graph not yet implemented");
+  }
+
+}
